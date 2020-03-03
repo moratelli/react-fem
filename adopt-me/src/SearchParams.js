490ac1f321062929d453af8dc9c7146d0c82962e
Pedro <pedromoratelli@gmail.com>
2020-03-03 11:12:06 -0300

    Criar Componente de Busca, Criar Hooks, Chamar Pet API

---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
